f3ce48c9c11b9a1e7a1317dd2cbc70071d6d0f88
DerickNorlan <dnorlan01@student.ciputra.ac.id>
2024-11-22 09:24:23 +0800

    update rp

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -28,34 +28,45 @@ class DetailPage extends StatelessWidget {
         // Judul aplikasi di AppBar menggunakan nama produk
         title: Text(
           productName,
-          style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.bold), // Menggunakan font Poppins untuk judul
+          style: GoogleFonts.poppins(
+              fontSize: 20,
+              fontWeight:
+                  FontWeight.bold), // Menggunakan font Poppins untuk judul
         ),
-        backgroundColor: kShrineBlue100, // Menggunakan warna yang sudah didefinisikan dalam colors.dart
+        backgroundColor:
+            kShrineBlue100, // Menggunakan warna yang sudah didefinisikan dalam colors.dart
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0), // Memberikan padding sekitar body halaman
+        padding: const EdgeInsets.all(
+            16.0), // Memberikan padding sekitar body halaman
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // Menyusun elemen-elemen secara vertikal dan rata kiri
+          crossAxisAlignment: CrossAxisAlignment
+              .start, // Menyusun elemen-elemen secara vertikal dan rata kiri
           children: [
             // Menampilkan gambar produk
             Center(
               child: Image.asset(
                 imageUrl, // Menampilkan gambar produk berdasarkan URL yang diterima
                 height: 200, // Menentukan tinggi gambar
-                fit: BoxFit.cover, // Mengatur agar gambar tetap sesuai dengan ukuran yang diberikan
+                fit: BoxFit
+                    .cover, // Mengatur agar gambar tetap sesuai dengan ukuran yang diberikan
               ),
             ),
-            const SizedBox(height: 20.0), // Memberikan jarak antara gambar dan teks
+            const SizedBox(
+                height: 20.0), // Memberikan jarak antara gambar dan teks
             // Menampilkan nama produk
             Text(
               productName,
               style: GoogleFonts.poppins(
                 fontSize: 24, // Ukuran font untuk nama produk
                 fontWeight: FontWeight.bold, // Membuat teks menjadi tebal
-                color: kShrineBlue500, // Menggunakan warna biru yang sudah didefinisikan
+                color:
+                    kShrineBlue500, // Menggunakan warna biru yang sudah didefinisikan
               ),
             ),
-            const SizedBox(height: 10.0), // Memberikan jarak antara nama produk dan deskripsi
+            const SizedBox(
+                height:
+                    10.0), // Memberikan jarak antara nama produk dan deskripsi
             // Menampilkan deskripsi produk
             Text(
               productDescription,
@@ -64,14 +75,17 @@ class DetailPage extends StatelessWidget {
                 color: Colors.grey[700], // Warna abu-abu untuk teks deskripsi
               ),
             ),
-            const SizedBox(height: 20.0), // Memberikan jarak antara deskripsi dan harga produk
+            const SizedBox(
+                height:
+                    20.0), // Memberikan jarak antara deskripsi dan harga produk
             // Menampilkan harga produk dengan format angka yang sesuai
             Text(
-              'Price: Rp. ${NumberFormat('#,##0').format(price)}', // Format harga dengan pemisah ribuan
+              'Price: \$ ${NumberFormat('#,##0').format(price)}', // Format harga dengan pemisah ribuan
               style: GoogleFonts.poppins(
                 fontSize: 18, // Ukuran font untuk harga
                 fontWeight: FontWeight.w600, // Menebalkan font harga
-                color: const Color.fromARGB(255, 68, 108, 192), // Warna biru untuk harga
+                color: const Color.fromARGB(
+                    255, 68, 108, 192), // Warna biru untuk harga
               ),
             ),
             const Spacer(), // Menambahkan ruang kosong agar harga selalu berada di bagian bawah
